2d9ae8e81e090f7b699d65d6dc138f3c28e96559
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-02 14:19:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ MODEL_FILE_PATH = "penguin_random_forest_model.pkl"
 st.title('🤖 Machine Learning App')
 
 st.info(
-    '''This is app builds a machine learning model in Python with streamlit! \n
+    '''This is app builds a machine learning model in Python with streamlit!\n
     Source: 
     https://github.com/dataprofessor/dp-machinelearning/blob/master/streamlit_app.py
     https://www.youtube.com/@streamlitofficial
