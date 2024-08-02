330c6d679f7e44a07ccb9e5c9e6da241e5e318d0
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-02 10:47:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,5 +17,5 @@ with st.expander('Data'):
   y=df['species']
   y
 with st.expander('Data visualization'):
-  st.scartter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
 
