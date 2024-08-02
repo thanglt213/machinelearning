b990190b9467e863ae8404af060d8020e1f098b5
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-02 14:12:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 # File path for saving the trained model
 MODEL_FILE_PATH = "penguin_random_forest_model.pkl"
 
-st.title('🤖 A Machine Learning App')
+st.title('🤖 Machine Learning App')
 
 st.info('''This is app builds a machine learning model in Python with streamlit! 
     Source: https://github.com/dataprofessor/dp-machinelearning/blob/master/streamlit_app.py
