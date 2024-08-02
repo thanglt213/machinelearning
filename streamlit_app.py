a5c9a46d397963e2d76407a0801e5001826adce9
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-02 10:26:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,6 @@ st.title('🎈 Machine learning app')
 
 st.info('This is a machine learning app')
 
-df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+# df=pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+df=pd.read_csv("https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv")
 df
