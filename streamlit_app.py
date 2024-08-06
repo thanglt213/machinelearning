80ed6877bd5e0f62a76cce78a4267045203a42f6
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-06 16:42:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ with st.sidebar:
     if st.button('Add'):
         # Combine with existing raw data (assuming X_raw is already defined)
         new_row = pd.DataFrame(data, index=[0])
-        input_df = pd.concat(input_df, new_row, ignore_index=True)
+        input_df = pd.concat([input_df, new_row], ignore_index=True)
 
 st.write(input_df)
 st.write(input_penguins)
