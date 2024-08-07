fd9945c27bc10d44277cb9419ace4f025f65b0bf
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-07 17:03:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,59 +67,7 @@ def get_user_input():
 
     return st.session_state.input_df
 
-# Show raw data
-with st.expander('Data'):
-    st.write('**Raw data**')
-    df, X_raw, y_raw = load_data()
-
-    st.write('**X**')
-    X_raw = df.drop('species', axis=1)
-    X_raw
-
-    st.write('**y**')
-    y_raw = df.species
-    y_raw
-
-with st.expander('Data visualization'):
-    st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
-
-# Initialize the session state to store the input data
-if 'input_df' not in st.session_state:
-    st.session_state['input_df'] = pd.DataFrame()
-    input_df=st.session_state['input_df']
-if 'input_penguins' not in st.session_state:
-    st.session_state['input_penguins'] = pd.DataFrame()
-    input_penguins=st.session_state['input_penguins']
-
-with st.sidebar:
-    st.header('Input features')
-    # get_user_input()
-    input_df = get_user_input()
-input_penguins = pd.concat([input_df, X_raw], axis=0)
-
-with st.expander('Input features'):
-    st.write('**Input penguin**')
-    input_df
-    st.write('**Combined penguins data**')
-    input_penguins
-
-# Data preparation
-encode = ['island', 'sex']
-df_penguins = pd.get_dummies(input_penguins, prefix=encode)
-
-n = len(input_df)
-X = df_penguins[n:]
-input_row = df_penguins[:n]
-
-target_mapper = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
-y = y_raw.apply(lambda val: target_mapper[val])
-
-with st.expander('Data preparation'):
-    st.write('**Encoded X (input penguin)**')
-    input_row
-    st.write('**Encoded y**')
-    y
-
+# Make predictions function
 def predict_penguin(clf, input_row: pd.DataFrame):
     if input_row.empty:
         st.write("Input data to predict!")
@@ -139,11 +87,68 @@ def predict_penguin(clf, input_row: pd.DataFrame):
         # Display the DataFrame with prediction probabilities and predicted species
         st.subheader('Prediction Probabilities and Predicted Species')
         st.dataframe(df_prediction_proba, column_config={
-            'Adelie': st.column_config.ProgressColumn('Adelie', format='%f', width='medium', min_value=0, max_value=1),
-            'Chinstrap': st.column_config.ProgressColumn('Chinstrap', format='%f', width='medium', min_value=0, max_value=1),
-            'Gentoo': st.column_config.ProgressColumn('Gentoo', format='%f', width='medium', min_value=0, max_value=1),
+            'Adelie': st.column_config.ProgressColumn('Adelie', format='%f', min_value=0, max_value=1),
+            'Chinstrap': st.column_config.ProgressColumn('Chinstrap', format='%f', min_value=0, max_value=1),
+            'Gentoo': st.column_config.ProgressColumn('Gentoo', format='%f', min_value=0, max_value=1),
             'Predicted_Species': st.column_config.TextColumn('Predicted Species', width='medium')
         }, hide_index=False)
+
+# Initialize the session state to store the input data
+if 'input_df' not in st.session_state:
+    st.session_state['input_df'] = pd.DataFrame()
+    input_df=st.session_state['input_df']
+if 'input_penguins' not in st.session_state:
+    st.session_state['input_penguins'] = pd.DataFrame()
+    input_penguins=st.session_state['input_penguins']
+
+# Show raw data
+with st.expander('Data'):
+    st.write('**Raw data**')
+    df, X_raw, y_raw = load_data()
+
+    st.write('**X**')
+    X_raw = df.drop('species', axis=1)
+    X_raw
+
+    st.write('**y**')
+    y_raw = df.species
+    y_raw
+
+# Data visualization
+with st.expander('Data visualization'):
+    st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+
+# Input features to predict
+with st.sidebar:
+    st.header('Input features')
+    # get_user_input()
+    input_df = get_user_input()
+
+# Show input features
+with st.expander('Input features'):
+    st.write('**Input penguin**')
+    input_df
+    st.write('**Combined penguins data**')
+    input_penguins = pd.concat([input_df, X_raw], axis=0)
+    input_penguins
+
+# Data preparation
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+
+n = len(input_df)
+X = df_penguins[n:]
+input_row = df_penguins[:n]
+
+target_mapper = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
+y = y_raw.apply(lambda val: target_mapper[val])
+
+# Show encoded features
+with st.expander('Data preparation'):
+    st.write('**Encoded X (input penguin)**')
+    input_row
+    st.write('**Encoded y**')
+    y
 
 # Load model
 clf = get_model()
