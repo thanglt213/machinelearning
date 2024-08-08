d5058281aafe54a706ac8f16ffbea2c51e55df21
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-08-08 11:43:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,23 +13,22 @@ st.title('🤖 Machine Learning App to Predict Penguins')
 
 # Display information about the app
 st.info(
-    '''
-    This app builds a machine learning model in Python with Streamlit!
+    '''This is app builds a machine learning model in Python with streamlit!
     \n Source: 
     https://github.com/dataprofessor/dp-machinelearning/blob/master/streamlit_app.py
     https://www.youtube.com/@streamlitofficial
     '''
 )
 
-# Function to load data from a CSV file
+# Function to load data from CSV file
 @st.cache
 def load_data():
     df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
     return df
 
-# Function to load or train the machine learning model
+# Function to load or train model
 @st.cache
-def get_model(X1, y1):
+def get_model(X1,y1):
     # Check if model file exists
     if os.path.exists(MODEL_FILE_PATH):
         # Load the model from file
@@ -43,7 +42,7 @@ def get_model(X1, y1):
         st.success("Model trained and saved to file.")
     return model
 
-# Function to get user input for prediction
+# Function to get user input
 def get_user_input():
     island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
     bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
@@ -59,52 +58,68 @@ def get_user_input():
         'body_mass_g': body_mass_g,
         'sex': gender
     }
-    return pd.DataFrame(data, index=[0])
+    # Add button to append data to the session state
+    if st.button('Add data to predict'):
+        # Combine with existing raw data (assuming X_raw is already defined)
+        new_row = pd.DataFrame(data, index=[0])
+        input_df = pd.concat([st.session_state.input_df, new_row], ignore_index=True)
+        st.session_state.input_df = input_df
 
+    return st.session_state.input_df
+
+# Data preparation
 # Function to encode categorical features
-def encode_features(X_raw, y_raw, input_df):
-    # Encoding islands
-    islands = ['Biscoe', 'Dream', 'Torgersen']
-    island_encoder = {island: i for i, island in enumerate(islands)}
-    X_raw['island'] = X_raw['island'].map(island_encoder)
-    input_df['island'] = input_df['island'].map(island_encoder)
+def encode_features(X_raw, y_raw, input_features):
 
-    # Encoding sex
-    genders = ['male', 'female']
-    gender_encoder = {gender: i for i, gender in enumerate(genders)}
-    X_raw['sex'] = X_raw['sex'].map(gender_encoder)
-    input_df['sex'] = input_df['sex'].map(gender_encoder)
+    # Encoding islands, sex
+    encode = ['island', 'sex']
+
+    features = pd.concat([input_features, X_raw], axis=0)
+    df_features = pd.get_dummies(features, prefix=encode)
+
+    n = len(input_features)
+    X = df_features[n:]
+    input_rows = df_features[:n]
 
     # Encoding species
-    species = ['Adelie', 'Chinstrap', 'Gentoo']
-    species_encoder = {species: i for i, species in enumerate(species)}
-    y_raw = y_raw.map(species_encoder)
+    target_mapper = {'Adelie': 0, 'Chinstrap': 1, 'Gentoo': 2}
+    y = y_raw.apply(lambda val: target_mapper[val])
+       
+    return X, y, input_rows
 
-    return X_raw, y_raw, input_df
-
-# Function to make predictions and display the results
-def predict_penguin(model, input_df):
-    prediction_proba = model.predict_proba(input_df)
-    prediction = model.predict(input_df)
-    df_prediction_proba = pd.DataFrame(prediction_proba, columns=['Adelie', 'Chinstrap', 'Gentoo'])
-    df_prediction_proba['Predicted_Species'] = prediction
-
-    # Display prediction probabilities and predicted species
-    st.subheader('Prediction Probabilities and Predicted Species')
-    st.dataframe(df_prediction_proba, column_config={
-        'Adelie': st.column_config.ProgressColumn('Adelie', format='%f', min_value=0, max_value=1),
-        'Chinstrap': st.column_config.ProgressColumn('Chinstrap', format='%f', min_value=0, max_value=1),
-        'Gentoo': st.column_config.ProgressColumn('Gentoo', format='%f', min_value=0, max_value=1),
-        'Predicted_Species': st.column_config.TextColumn('Predicted Species', width='medium')
-    }, hide_index=False)
+# Function to make predictions and display results
+def predict_penguin(clf, input_rows: pd.DataFrame):
+    if input_rows.empty:
+        st.write("Input data to predict!")
+    else:
+        prediction = clf.predict(input_rows)
+        prediction_proba = clf.predict_proba(input_rows)
+        
+        # DataFrame containing prediction probabilities for each species
+        df_prediction_proba = pd.DataFrame(prediction_proba, columns=['Adelie', 'Chinstrap', 'Gentoo'])
+        
+        # Array containing the names of the penguin species
+        penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
+        
+        # Add 'Predicted_Species' column to the DataFrame df_prediction_proba
+        df_prediction_proba['Predicted_Species'] = [penguins_species[pred] for pred in prediction]
+        
+        # Display the DataFrame with prediction probabilities and predicted species
+        st.subheader('Prediction Probabilities and Predicted Species')
+        st.dataframe(df_prediction_proba, column_config={
+            'Adelie': st.column_config.ProgressColumn('Adelie', format='%f', min_value=0, max_value=1),
+            'Chinstrap': st.column_config.ProgressColumn('Chinstrap', format='%f', min_value=0, max_value=1),
+            'Gentoo': st.column_config.ProgressColumn('Gentoo', format='%f', min_value=0, max_value=1),
+            'Predicted_Species': st.column_config.TextColumn('Predicted Species', width='medium')
+        }, hide_index=False)
 
 # Initialize the session state to store the input data
 if 'input_df' not in st.session_state:
     st.session_state['input_df'] = pd.DataFrame()
-    input_df = st.session_state['input_df']
+    input_df=st.session_state['input_df']
 if 'input_penguins' not in st.session_state:
     st.session_state['input_penguins'] = pd.DataFrame()
-    input_penguins = st.session_state['input_penguins']
+    input_penguins=st.session_state['input_penguins']
 
 # Show raw data
 with st.expander('Data'):
@@ -113,11 +128,11 @@ with st.expander('Data'):
 
     st.write('**X**')
     X_raw = df.drop('species', axis=1)
-    st.dataframe(X_raw)
+    X_raw
 
     st.write('**y**')
-    y_raw = df['species']
-    st.dataframe(y_raw)
+    y_raw = df.species
+    y_raw
 
 # Data visualization
 with st.expander('Data visualization'):
@@ -131,22 +146,24 @@ with st.sidebar:
 # Show input features
 with st.expander('Input features'):
     st.write('**Input penguin**')
-    st.dataframe(input_df)
+    input_df
 
-# Encode features
-X, y, input_df = encode_features(X_raw, y_raw, input_df)
+# Encode categorical features
+X, y, input = encode_features(X_raw, y_raw, input_df)
 
-# Show encoded features
+# Show encoded categorical features
 with st.expander('Data preparation'):
     st.write('**Encoded input penguins**')
-    st.dataframe(input_df)
+    input
     st.write('**Encoded X**')
-    st.dataframe(X)
+    X
     st.write('**Encoded y**')
-    st.dataframe(y)
+    y
 
 # Load model
-clf = get_model(X, y)
+clf = get_model(X,y)
 
 # Make prediction
-predict_penguin(clf, input_df)
+predict_penguin(clf, input)
+
+
